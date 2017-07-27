a7da66851b5d7e75c3373501875478c842565f52
Hassan Naeem <hnae6443@gmail.com>
2017-07-27 15:40:57 -0400

    Added completed functionality

--- src/components/GoalItem.js
+++ src/components/GoalItem.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { goalRef, completeGoalRef } from '../firebase';
 
 class GoalItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    }
+  }
+
+  completeGoal() {
+    const { email, title, serverKey } = this.props.goal;
+    if (email === this.props.user.email) {
+      completeGoalRef.push({email, title});
+      goalRef.child(serverKey).remove();
+    } else {
+      this.setState({error: "This goal hasn't been made by you!"});
+    }
+  };
+
   render() {
     const { email, title } = this.props.goal;
 
@@ -8,9 +27,28 @@ class GoalItem extends Component {
       <div>
         <strong>{title}</strong>
         <span>made by <em>{email}</em></span>
+        <button
+          className="btn"
+          onClick={() => this.completeGoal()}
+        >
+            Complete
+        </button>
+        <button
+          className="btn"
+        >
+            Delete
+        </button>
+        <div>{this.state.error}</div>
       </div>
     )
   }
 }
 
-export default GoalItem;
+function mapStateToProps(state) {
+  const { user } = state;
+  return {
+    user
+  }
+}
+
+export default connect(mapStateToProps, null)(GoalItem);
